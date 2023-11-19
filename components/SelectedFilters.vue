840c9d5e9120eec0ed72dadc66f7bcc409d0bd85
Daniel Olguin <dani.olg@hotmail.com>
2023-11-18 22:45:28 -0300

    add selected filters component

---
+++ components/SelectedFilters.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    v-if="filters.length"
+    class="flex justify-between items-center bg-white p-5 rounded shadow-xl mb-4"
+  >
+    <div class="grid grid-cols-2 gap-2">
+      <Tag
+        v-for="filter in filters"
+        :name="filter"
+        clearable
+      />
+    </div>
+    <div
+      @click="clearFilters"
+      class="text-desaturatedDarkCyan font-bold hover:cursor-default hover:underline"
+    >Clear</div>
+  </div>
+</template>
+<script>
+import { mapActions, mapState } from "pinia";
+import { useFiltersStore } from "@/stores/filters.js";
+
+export default {
+  computed: {
+    ...mapState(useFiltersStore, ['filters'])
+  },
+  methods: {
+    ...mapActions(useFiltersStore, ['clearFilters'])
+  }
+}
+</script>
